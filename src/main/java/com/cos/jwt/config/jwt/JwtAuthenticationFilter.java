fa2b8a1052a050c52b5af47de0d516fc3b2a5609
SungJaeCho <liadus@naver.com>
2021-06-05 06:50:36 +0900

    jwt생성

--- src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -1,21 +1,26 @@
 package com.cos.jwt.config.jwt;
 
-import com.cos.jwt.config.auth.PrincipalDetails;
-import com.cos.jwt.model.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.IOException;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.cos.jwt.config.auth.PrincipalDetails;
+import com.cos.jwt.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
 
 //스프링 시큐리티에서 UsernamePasswordAuthenticationFilter 가 있음
 // /login 오청해서 username, password 전송하면(POST)
@@ -70,6 +75,16 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         System.out.println("successfulAuthentication 실행됨 인증완료");
-        super.successfulAuthentication(request, response, chain, authResult);
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+        
+        // RSA방식은 아니고 Hash암호방식
+        String jwtToken = JWT.create()
+        		.withSubject("cos토큰")
+        		.withExpiresAt(new Date(System.currentTimeMillis()+(60000*10)))
+        		.withClaim("id", principalDetails.getUser().getId())
+        		.withClaim("username", principalDetails.getUser().getUsername())
+        		.sign(Algorithm.HMAC512("cos"))
+        		;
+        response.addHeader("Authorization", "Bearer "+jwtToken);
     }
 }
